7f6d408583f0881c767d048be340e9c196fd6b2e
Tyler Hill <gotyhill@gmail.com>
2022-12-27 03:24:21 +0000

    Fetch API

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let path = '/recipes/recipes/';///recipes/recipes/
-import { Recipe } from '/recipes/js/parse.js';///recipes/js/parse.js
+let path = '/recipes/recipes/';// /recipes/recipes/
+import { Recipe } from '/recipes/js/parse.js';// /recipes/js/parse.js
 
 
 
@@ -74,166 +74,156 @@ function isValidUrl(urlString) {
 }
 
 function openRecipe(name) {
-  const request = new XMLHttpRequest();
-  request.onload = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      //add title
-      modal.getElementsByTagName('h2')[0].innerText = name;
-      //parse
-      const parsed = Recipe(request.response);
-      //add metadata
-      const table = modal.getElementsByTagName('table')[0];
-      while (table.firstChild) {
-        table.removeChild(table.firstChild);
-      }
-      if (parsed.metadata) {
-        table.style.display = 'block';
-        for (const metadata in parsed.metadata) {
-          const tr = document.createElement('tr');
-          const td1 = document.createElement('td');
-          td1.innerText = metadata;
-          tr.append(td1);
-          const td2 = document.createElement('td');
-          if (metadata.toLowerCase() === 'source' && isValidUrl(parsed.metadata[metadata])) {
-            const a = document.createElement('a');
-            a.target = '_blank';
-            a.rel = 'noopener';
-            a.innerText = parsed.metadata[metadata];
-            a.href = parsed.metadata[metadata];
-            td2.append(a);
-          } else {
-            td2.innerText = parsed.metadata[metadata];
-          }
-          tr.append(td2);
-          table.append(tr);
-        }
-      } else {
-        table.style.display = 'none';
-      }
-      const ingedientCookware = modal.getElementsByClassName('ingedientCookware')[0];
-      const ul1 = modal.getElementsByTagName('ul')[0];
-      while (ul1.firstChild) {
-        ul1.removeChild(ul1.firstChild);
-      }
-      const ul2 = modal.getElementsByTagName('ul')[1];
-      while (ul2.firstChild) {
-        ul2.removeChild(ul2.firstChild);
-      }
-      if (parsed.ingredients || parsed.cookware) {
-        ingedientCookware.style.display = 'flex';
-        //add ingredients
-        if (parsed.ingredients) {
-          ul1.style.display = 'block';
-          for (const ingredient of parsed.ingredients) {
-            const li = document.createElement('li');
-            const input = document.createElement('input');
-            input.type = 'checkbox';
-            li.append(input);
-            if (ingredient.quantity) {
-              const span = document.createElement('span');
-              span.classList.add('amount');
-              span.innerText = ingredient.quantity + ' ' + ingredient.units;
-              li.append(span);
-              li.append(createTextSpan(' '));
-            }
-            li.append(createTextSpan(ingredient.name));
-            ul1.append(li);
-          }
-        } else {
-          ul1.style.display = 'none';
-        }
-        //add cookware
-        if (parsed.cookware) {
-          ul2.style.display = 'block';
-          for (const cookware of parsed.cookware) {
-            const li = document.createElement('li');
-            if (cookware.quantity) {
-              const span = document.createElement('span');
-              span.classList.add('amount');
-              span.innerText = cookware.quantity;
-              li.append(span);
-              li.append(createTextSpan(' '));
-            }
-            li.append(createTextSpan(cookware.name));
-            ul2.append(li);
-          }
-        } else {
-          ul2.style.display = 'none';
-        }
-      } else {
-        ingedientCookware.style.display = 'none';
-      }
-      //add steps
-      const ol = modal.getElementsByTagName('ol')[0];
-      while (ol.firstChild) {
-        ol.removeChild(ol.firstChild);
-      }
-      if (parsed.steps) {
-        ol.style.display = 'block';
-        for (const step of parsed.steps) {
-          const li = document.createElement('li');
-          for (const subStep of step) {
-            if (subStep.type === 'text') {
-              li.append(createTextSpan(subStep.value));
-            } else if (subStep.type === 'ingredient') {
-              const span = document.createElement('span');
-              span.classList.add('ingredient');
-              span.innerText = subStep.name;
-              if (subStep.quantity) {
-                span.title = subStep.quantity + ' ' + subStep.units;
-              }
-              li.append(span);
-            } else if (subStep.type === 'cookware') {
-              const span = document.createElement('span');
-              span.classList.add('cookware');
-              span.innerText = subStep.name;
-              if (subStep.quantity) {
-                span.title = subStep.quantity;
-              }
-              li.append(span);
-            } else if (subStep.type === 'timer') {
-              const span = document.createElement('span');
-              span.classList.add('timer');
-              span.innerText = subStep.quantity;
-              if (subStep.units) {
-                span.innerText += ' ' + subStep.units;
-              }
-              if (subStep.name) {
-                span.title = subStep.name;
-              }
-              li.append(span);
-            }
-          }
-          ol.append(li);
-        }
-      } else {
-        ol.style.display = 'none';
-      }
-      //show modal
-      modal.style.display = 'block';
-      document.getElementById('recipe').scrollTop = 0;
-      disableScroll();
-      modal.classList.remove('out');
-    } else if (this.readyState === 4 && this.status !== 200) {
-      Error(request.statusText);
+  fetch(path + name + '.cook').then(async (response) => {
+    if (response.ok) {
+      return response.text();
+    } else {
+      throw await response.text();
     }
-  };
-  request.onerror = function () {
-    Error('Network Error');
-  };
-  request.open('GET', path + name + '.cook');
-  request.send();
+  }).then((data) => {
+    //add title
+    modal.getElementsByTagName('h2')[0].innerText = name;
+    //parse
+    const parsed = Recipe(data);
+    //add metadata
+    const table = modal.getElementsByTagName('table')[0];
+    while (table.firstChild) {
+      table.removeChild(table.firstChild);
+    }
+    if (parsed.metadata) {
+      table.style.display = 'block';
+      for (const metadata in parsed.metadata) {
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        td1.innerText = metadata;
+        tr.append(td1);
+        const td2 = document.createElement('td');
+        if (metadata.toLowerCase() === 'source' && isValidUrl(parsed.metadata[metadata])) {
+          const a = document.createElement('a');
+          a.target = '_blank';
+          a.rel = 'noopener';
+          a.innerText = parsed.metadata[metadata];
+          a.href = parsed.metadata[metadata];
+          td2.append(a);
+        } else {
+          td2.innerText = parsed.metadata[metadata];
+        }
+        tr.append(td2);
+        table.append(tr);
+      }
+    } else {
+      table.style.display = 'none';
+    }
+    const ingedientCookware = modal.getElementsByClassName('ingedientCookware')[0];
+    const ul1 = modal.getElementsByTagName('ul')[0];
+    while (ul1.firstChild) {
+      ul1.removeChild(ul1.firstChild);
+    }
+    const ul2 = modal.getElementsByTagName('ul')[1];
+    while (ul2.firstChild) {
+      ul2.removeChild(ul2.firstChild);
+    }
+    if (parsed.ingredients || parsed.cookware) {
+      ingedientCookware.style.display = 'flex';
+      //add ingredients
+      if (parsed.ingredients) {
+        ul1.style.display = 'block';
+        for (const ingredient of parsed.ingredients) {
+          const li = document.createElement('li');
+          const input = document.createElement('input');
+          input.type = 'checkbox';
+          li.append(input);
+          if (ingredient.quantity) {
+            const span = document.createElement('span');
+            span.classList.add('amount');
+            span.innerText = ingredient.quantity + ' ' + ingredient.units;
+            li.append(span);
+            li.append(createTextSpan(' '));
+          }
+          li.append(createTextSpan(ingredient.name));
+          ul1.append(li);
+        }
+      } else {
+        ul1.style.display = 'none';
+      }
+      //add cookware
+      if (parsed.cookware) {
+        ul2.style.display = 'block';
+        for (const cookware of parsed.cookware) {
+          const li = document.createElement('li');
+          if (cookware.quantity) {
+            const span = document.createElement('span');
+            span.classList.add('amount');
+            span.innerText = cookware.quantity;
+            li.append(span);
+            li.append(createTextSpan(' '));
+          }
+          li.append(createTextSpan(cookware.name));
+          ul2.append(li);
+        }
+      } else {
+        ul2.style.display = 'none';
+      }
+    } else {
+      ingedientCookware.style.display = 'none';
+    }
+    //add steps
+    const ol = modal.getElementsByTagName('ol')[0];
+    while (ol.firstChild) {
+      ol.removeChild(ol.firstChild);
+    }
+    if (parsed.steps) {
+      ol.style.display = 'block';
+      for (const step of parsed.steps) {
+        const li = document.createElement('li');
+        for (const subStep of step) {
+          if (subStep.type === 'text') {
+            li.append(createTextSpan(subStep.value));
+          } else if (subStep.type === 'ingredient') {
+            const span = document.createElement('span');
+            span.classList.add('ingredient');
+            span.innerText = subStep.name;
+            if (subStep.quantity) {
+              span.title = subStep.quantity + ' ' + subStep.units;
+            }
+            li.append(span);
+          } else if (subStep.type === 'cookware') {
+            const span = document.createElement('span');
+            span.classList.add('cookware');
+            span.innerText = subStep.name;
+            if (subStep.quantity) {
+              span.title = subStep.quantity;
+            }
+            li.append(span);
+          } else if (subStep.type === 'timer') {
+            const span = document.createElement('span');
+            span.classList.add('timer');
+            span.innerText = subStep.quantity;
+            if (subStep.units) {
+              span.innerText += ' ' + subStep.units;
+            }
+            if (subStep.name) {
+              span.title = subStep.name;
+            }
+            li.append(span);
+          }
+        }
+        ol.append(li);
+      }
+    } else {
+      ol.style.display = 'none';
+    }
+    //show modal
+    modal.style.display = 'block';
+    document.getElementById('recipe').scrollTop = 0;
+    disableScroll();
+    modal.classList.remove('out');
+  });
 }
 
 
 
 /* Masonry */
-
-document.body.addEventListener('keydown', function (e) {//enable enter while tabbing over spans
-  if ((e.key === 'Enter' || e.key === ' ' || e.key === 'Spacebar') && document.activeElement !== null && document.activeElement.classList.contains('item')) {
-    document.activeElement.click();
-  }
-});
 
 const recipes = document.getElementsByClassName('item');
 for (let i = 0; i < recipes.length; i++) {
